34684fea2855a53eb1445860b0fe575f577bc2d3
Alexandru Piteli <piteli@etone.de>
2023-10-30 16:16:21 +0100

    mobile menu opens and closes, with animation

--- src/organisms/navbar/navbar.tsx
+++ src/organisms/navbar/navbar.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../atoms/icons/logo";
 import MobileMenuButton from "../../molecules/menuButton/mobileMenuButton";
 import MenuItems from "../../molecules/menuItems/menuItems";
+import MenuThemaSwitch from "../../molecules/menuThemaSwitch/menuThemaSwitch";
+import MenuUserDropdown from "../../molecules/menuUserDropdown/menuUserDropdown";
+import "./navbar.scss";
 
 export default () => {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
+
     return <nav className="bg-gray-800">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-                <MobileMenuButton open={false}/>
+                <div onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+                    <MobileMenuButton open={mobileMenuOpen} />
+                </div>
                 <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                     <Logo />
                     <div className="hidden sm:ml-6 sm:block">
@@ -17,47 +24,13 @@ export default () => {
                     </div>
                 </div>
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                        <span className="absolute -inset-1.5"></span>
-                        <span className="sr-only">View notifications</span>
-                        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-                        </svg>
-                    </button>
-
-                    {/* <!-- Profile dropdown --> */}
-                    <div className="relative ml-3">
-                        <div>
-                            <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                <span className="absolute -inset-1.5"></span>
-                                <span className="sr-only">Open user menu</span>
-                                <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                            </button>
-                        </div>
-
-                        {/* <!--
-                            Dropdown menu, show/hide based on menu state.
-
-                            Entering: "transition ease-out duration-100"
-                            From: "transform opacity-0 scale-95"
-                            To: "transform opacity-100 scale-100"
-                                Leaving: "transition ease-in duration-75"
-                            From: "transform opacity-100 scale-100"
-                            To: "transform opacity-0 scale-95"
-                        --> */}
-                        <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
-                            {/* <!-- Active: "bg-gray-100", Not Active: "" --> */}
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-0">Your Profile</a>
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-1">Settings</a>
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-2">Sign out</a>
-                        </div>
-                    </div>
+                    <MenuThemaSwitch />
+                    <MenuUserDropdown />
                 </div>
             </div>
         </div>
 
-        {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-        <div className="sm:hidden" id="mobile-menu">
+        <div className={"mobile-menu sm:hidden" + (mobileMenuOpen ? "" : " hide")} id="mobile-menu">
             <div className="space-y-1 px-2 pb-3 pt-2">
                 <MenuItems current="team" mobile={true} />
             </div>
