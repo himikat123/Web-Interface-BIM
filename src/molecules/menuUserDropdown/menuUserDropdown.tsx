773557081c7c419a05adf192f5494e3639581727
Alexandru Piteli <piteli@etone.de>
2023-10-31 10:38:40 +0100

    dark mode & current url

--- src/molecules/menuUserDropdown/menuUserDropdown.tsx
+++ src/molecules/menuUserDropdown/menuUserDropdown.tsx
@@ -1,9 +1,10 @@
 import React, { useRef, useEffect, useState } from "react";
 import MenuUserItem from "../../atoms/menuUserItem/menuUserItem";
 import "./menuUserDropdown.scss";
+import { iMenuUserDropdown } from "../../interfaces";
 
-export default () => {
-    function useOutsideAlerter(ref: any) {
+export default (props: iMenuUserDropdown) => {
+    function useOutsideAlerter(ref: React.RefObject<HTMLDivElement>) {
         useEffect(() => {
             function handleClickOutside(event: any) {
                 if(ref.current && !ref.current.contains(event.target)) setMenuUserOpen(false);
@@ -16,7 +17,7 @@ export default () => {
     }
 
     const [menuUserOpen, setMenuUserOpen] = useState<boolean>(false);
-    const wrapperRef = useRef(null);
+    const wrapperRef = useRef<HTMLDivElement>(null);
     useOutsideAlerter(wrapperRef);
     
     return (
@@ -29,15 +30,15 @@ export default () => {
                 </button>
             </div>
 
-            <div className={"absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none menu-user-dropdown" + (menuUserOpen ? "" : " hide")}
+            <div className={"absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white dark:bg-gray-700 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none menu-user-dropdown" + (menuUserOpen ? "" : " hide")}
               role="menu" 
               aria-orientation="vertical" 
               aria-labelledby="user-menu-button" 
               tabIndex={-1}
             >
-                <MenuUserItem link="profile" current="settings" title="Your Profile" num={0} />
-                <MenuUserItem link="settings" current="settings" title="Settings" num={1} />
-                <MenuUserItem link="signout" current="settings" title="Sign out" num={2} />
+                <MenuUserItem link="profile" current={props.current} title="Your Profile" num={0} />
+                <MenuUserItem link="settings" current={props.current} title="Settings" num={1} />
+                <MenuUserItem link="signout" current={props.current} title="Sign out" num={2} />
             </div>
         </div>
     )
