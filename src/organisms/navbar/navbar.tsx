233a774ff0dfef31102a477f2330da448f613987
Alexandru Piteli <piteli@etone.de>
2023-10-31 11:08:30 +0100

    highlighting menu items according to selected page

--- src/organisms/navbar/navbar.tsx
+++ src/organisms/navbar/navbar.tsx
@@ -22,6 +22,8 @@ export default () => {
     const wrapperRef = useRef<HTMLDivElement>(null);
     useOutsideAlerter(wrapperRef);
 
+    const currentPath = window.location.pathname.slice(1);
+
     return <nav ref={wrapperRef} className="bg-gray-300 dark:bg-gray-800">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
@@ -31,20 +33,20 @@ export default () => {
                 <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                     <div className="hidden sm:ml-6 sm:block">
                         <div className="flex space-x-4">
-                            <MenuItems current="team" mobile={false} />
+                            <MenuItems current={currentPath} mobile={false} />
                         </div>
                     </div>
                 </div>
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                     <MenuThemaSwitch />
-                    <MenuUserDropdown current="settings" />
+                    <MenuUserDropdown current={currentPath} />
                 </div>
             </div>
         </div>
 
         <div className={"mobile-menu sm:hidden" + (mobileMenuOpen ? "" : " hide")} id="mobile-menu">
             <div className="space-y-1 px-2 pb-3 pt-2">
-                <MenuItems current="team" mobile={true} />
+                <MenuItems current={currentPath} mobile={true} />
             </div>
         </div>
     </nav>
