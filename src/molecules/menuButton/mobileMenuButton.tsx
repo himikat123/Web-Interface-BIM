8fa94cbe8cef81c7ad6dcc27e1438a6414c9b0fb
Alexandru Piteli <piteli@etone.de>
2023-10-31 10:40:11 +0100

    mobile menu button size

--- src/molecules/menuButton/mobileMenuButton.tsx
+++ src/molecules/menuButton/mobileMenuButton.tsx
@@ -2,23 +2,24 @@ import React from "react";
 import { iMobileMenuButton } from "../../interfaces";
 import { ReactComponent as HamburgerSVG } from '../../atoms/icons/hamburger.svg';
 import { ReactComponent as CloseSVG } from '../../atoms/icons/close.svg';
+import "./mobileMenuButton.scss";
 
 export default (props: iMobileMenuButton) => {
     return (
         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <button type="button" 
-              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" 
+              className="relative inline-flex items-center justify-center rounded-md p-2 text-black dark:text-white hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" 
               aria-controls="mobile-menu" 
               aria-expanded={props.open ? "true" : "false"}
             >
                 <span className="absolute -inset-0.5"></span>
                 <span className="sr-only">Open main menu</span>
                 
-                <div className={props.open ? "hidden" : "block"}>
+                <div className={"mobile-menu-icon " + (props.open ? "hidden" : "block")}>
                     <HamburgerSVG />
                 </div>
 
-                <div className={props.open ? "block" : "hidden"}>
+                <div className={"mobile-menu-icon " + (props.open ? "block" : "hidden")}>
                     <CloseSVG />
                 </div>
             </button>
