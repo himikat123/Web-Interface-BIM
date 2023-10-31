c561e0940db8113bf5e58f6fe738d70bc1df833d
Alexandru Piteli <piteli@etone.de>
2023-10-31 14:49:55 +0100

    change colors

--- src/atoms/menuUserItem/menuUserItem.tsx
+++ src/atoms/menuUserItem/menuUserItem.tsx
@@ -4,7 +4,7 @@ import { iMenuUserItem } from "../../interfaces";
 export default (props: iMenuUserItem) => {
     return (
         <a href={props.link} 
-          className={"block px-4 py-2 text-sm text-black dark:text-white" + (props.current == props.link ? " bg-gray-200 dark:bg-gray-300 dark:text-gray-900" : "")} 
+          className={"block px-4 py-2 text-sm text-black dark:text-white" + (props.current == props.link ? " bg-gray-700 dark:bg-gray-300 text-white dark:text-black" : "")} 
           role="menuitem" 
           tabIndex={-1} 
           id={"user-menu-item-" + props.num}
