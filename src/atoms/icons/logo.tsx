ba60ee55dc1bcee9c45b99ce72eb743b93c0afb5
Alexandru Piteli <piteli@etone.de>
2023-10-31 10:37:39 +0100

    delete logo

--- src/atoms/icons/logo.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default () => {
-    return <div className="flex flex-shrink-0 items-center">
-        <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="BIM" />
-    </div>
-}
