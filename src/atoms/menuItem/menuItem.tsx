6c4c853833d0f7b6fce1354f2aa2465f30451113
Alexandru Piteli <piteli@etone.de>
2023-10-31 14:22:45 +0100

    change colors

--- src/atoms/menuItem/menuItem.tsx
+++ src/atoms/menuItem/menuItem.tsx
@@ -3,7 +3,7 @@ import { iMenuItem } from "../../interfaces";
 
 export default (props: iMenuItem) => {
     const passive = "text-gray-900 dark:text-gray-200 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 font-medium";
-    const active = "bg-gray-900 text-white rounded-md px-3 py-2 font-medium";
+    const active = "bg-gray-900 dark:bg-gray-300 text-white dark:text-gray-900 rounded-md px-3 py-2 font-medium";
     const desktop = " text-sm";
     const mobile = " text-base block";
 
