6b95db6682e9a7224daa8353456ace533ef7c7d9
Alex Him <himikat123@gmail.com>
2023-11-01 12:30:24 +0100

    chang size of switching to mobile view

--- src/molecules/menuMobileButton/menuMobileButton.tsx
+++ src/molecules/menuMobileButton/menuMobileButton.tsx
@@ -3,10 +3,8 @@ import { iMenuMobileButton } from "../../interfaces";
 
 export default (props: iMenuMobileButton) => {
     return (
-        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button type="button" 
-              className="relative inline-flex items-center justify-center rounded-md p-2 text-black dark:text-white hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-            >
+        <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
+            <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-black dark:text-white hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                 <span className="absolute -inset-0.5"></span>
                 {props.children}
             </button>
--- src/organisms/navbar/navbar.tsx
+++ src/organisms/navbar/navbar.tsx
@@ -27,28 +27,28 @@ export default () => {
     const currentPath = window.location.pathname;
 
     return <nav ref={wrapperRef} className="bg-gray-300 dark:bg-gray-800 navbar">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-7xl px-2 md:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
                 <div onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
                     <MenuMobileButton>
                         {mobileMenuOpen ? <CloseSVG /> : <HamburgerSVG />}
                     </MenuMobileButton>
                 </div>
-                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                    <div className="hidden sm:ml-6 sm:block">
+                <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+                    <div className="hidden md:ml-6 md:block">
                         <div className="flex space-x-4">
                             <MenuItems current={currentPath} mobile={false} />
                         </div>
                     </div>
                 </div>
-                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
                     <MenuThemaSwitch />
                     <MenuUserDropdown current={currentPath} />
                 </div>
             </div>
         </div>
 
-        <div className={"mobile-menu sm:hidden" + (mobileMenuOpen ? "" : " hide")} 
+        <div className={"mobile-menu md:hidden" + (mobileMenuOpen ? "" : " hide")} 
           id="mobile-menu"
           style={{overflow: mobileMenuOpen ? "visible" : "hidden"}}  
         >
