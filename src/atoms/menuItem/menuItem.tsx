4f1ed8981821302bbf80e6365ab6f0c39e05a265
Alex Him <himikat123@gmail.com>
2023-11-01 11:44:12 +0100

    submenu open/close

--- src/atoms/menuItem/menuItem.tsx
+++ src/atoms/menuItem/menuItem.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DropdownBox from "../dropdownBox/dropdownBox";
 import { iMenuItem } from "../../interfaces";
 
 export default (props: iMenuItem) => {
+    function useOutsideAlerter(ref: React.RefObject<HTMLDivElement>) {
+        useEffect(() => {
+            function handleClickOutside(event: any) {
+                if(ref.current && !ref.current.contains(event.target)) setSubMenuOpen(false);
+            }
+            document.addEventListener("mousedown", handleClickOutside);
+            return () => {
+                document.removeEventListener("mousedown", handleClickOutside);
+            };
+        }, [ref, subMenuOpen]);
+    }
+    const [subMenuOpen, setSubMenuOpen] = useState<boolean>(false);
+    const wrapperRef = useRef<HTMLDivElement>(null);
+    useOutsideAlerter(wrapperRef);
+
     const passive = "text-gray-900 dark:text-gray-200 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 font-medium";
     const active = "bg-gray-900 dark:bg-gray-300 text-white dark:text-gray-900 rounded-md px-3 py-2 font-medium";
     const desktop = " text-sm";
@@ -13,13 +28,20 @@ export default (props: iMenuItem) => {
         if(props.current == child.props.link) highlight = active;
     });
 
-    return (<div className="relative">
-        <a href={props.link} className={highlight + (props.mobile ? mobile : desktop)}>
-            {props.title}
-        </a>
+    return (
+        <div ref={wrapperRef} className="relative">
+            <div onClick={() => setSubMenuOpen(!subMenuOpen)}>
+                <a className={highlight + (props.mobile ? mobile : desktop)} 
+                  href={props.link}
+                  onClick={e => (props.children ? e.preventDefault() : null)}
+                >
+                    {props.title}
+                </a>
+            </div>
 
-        {props.children && <DropdownBox className="mt-2" open={true}>
-            {props.children}
-        </DropdownBox>}
-    </div>)
+            {props.children && <DropdownBox className="submenu-dropbox animate-vertical" open={subMenuOpen}>
+                {props.children}
+            </DropdownBox>}
+        </div>
+    )
 }
