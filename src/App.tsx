ebe501c6a90b98b68b771433c2bb67de4aded17a
Alex Him <himikat123@gmail.com>
2023-11-22 18:55:42 +0100

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom"
 import './App.css';
 import Navbar from './organisms/navbar/navbar';
 import LangSwitcher from './organisms/langSwitcher/langSwitcher';
 
 function App() {
-    const router = createBrowserRouter([
-        { path: "/",     element: <div>Homepage</div>, errorElement: <div>404 Page nui</div> },
-        { path: "/lang", element: <LangSwitcher /> },
-    ]);
+    const location = useLocation()
 
     return (<>
-        <Navbar />
+        {location.pathname != '/login' && <Navbar />}
 
-        <RouterProvider router={router} />
+        <Routes>
+            <Route path="/"     element={ <div>Homepage</div> }>    </Route>
+            <Route path="/lang" element={ <LangSwitcher /> }>       </Route>
+            <Route path="/*"    element={ <div>404 Page nui</div> }></Route>
+        </Routes>
     </>);
 }
 
