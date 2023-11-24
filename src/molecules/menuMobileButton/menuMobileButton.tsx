4c182adaf9e6149c3286a6e0dd61abe758f40417
Alexandru Piteli <piteli@etone.de>
2023-11-24 11:46:47 +0100

    colors adjust

--- src/molecules/menuMobileButton/menuMobileButton.tsx
+++ src/molecules/menuMobileButton/menuMobileButton.tsx
@@ -4,7 +4,7 @@ import { iMenuMobileButton } from "../../interfaces";
 const MenuMobileButton = (props: iMenuMobileButton) => {
     return (
         <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
-            <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-menu_dark dark:text-menu_light hover:bg-menu_dark hover:text-menu_light focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-text_dark hover:bg-bg-menu_active_light hover:bg-menu_active_dark focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                 <span className="absolute -inset-0.5"></span>
                 {props.children}
             </button>
