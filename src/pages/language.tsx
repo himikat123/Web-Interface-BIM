96a822fd69aa2918db4a5d6d3f2d61fb0298ca5e
Alexandru Piteli <piteli@etone.de>
2023-11-24 15:03:09 +0100

    add new pages

---
+++ src/pages/language.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import i18n from '../i18n/main';
+import TwoColumns from "../templates/twoColumns";
+import Card from "../atoms/card";
+
+const Language = () => {
+    const content = <>
+        <Card content={<div>1</div>} />
+        <Card content={<div>2</div>} />
+        <Card content={<div>3</div>} />
+        <Card content={<div>4</div>} />
+    </>;
+
+    return (<>
+        <TwoColumns header={i18n.t('status')} content={content} navbar={true} />
+    </>);
+}
+
+export default Language;
