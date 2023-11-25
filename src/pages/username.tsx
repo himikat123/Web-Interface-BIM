fa801df4725cb87d031e1a35399ed325c985799f
Alex Him <himikat123@gmail.com>
2023-11-25 21:40:11 +0100

    add username page

---
+++ src/pages/username.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import TwoColumns from "../templates/twoColumns";
+import { useSelector, useDispatch } from 'react-redux';
+import i18n from '../i18n/main';
+import Card from "../atoms/card";
+import { iState } from "../interfaces";
+
+const Username = () => {
+    const username = useSelector((state: iState) => state.config.account.name);
+    const dispatch = useDispatch();
+
+    const content = <>
+        <Card content={<>{username}</>} />
+        <Card content={<>{username}</>} />
+    </>;
+
+    return (<>
+        <TwoColumns header={i18n.t('username')} content={content} navbar={true} buttons={['save', 'reset']} />
+    </>);
+}
+
+export default Username;
