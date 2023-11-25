e54d55f2cff5d04a021747d6a267b297fc97f8da
Alex Him <himikat123@gmail.com>
2023-11-25 08:24:34 +0100

    removed maximum nav width limit

--- src/organisms/navbar.tsx
+++ src/organisms/navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
     const currentPath = window.location.pathname;
 
     return <nav ref={wrapperRef} className="bg-menu_light dark:bg-menu_dark navbar fixed w-full">
-        <div className="mx-auto max-w-7xl px-2 md:px-6 lg:px-8">
+        <div className="mx-auto px-2 md:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
                 <div onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
                     <MenuMobileButton>
