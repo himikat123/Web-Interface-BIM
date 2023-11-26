15ac9392b290b90041cb4ce1ca3878535eaf417e
Alex Him <himikat123@gmail.com>
2023-11-26 18:29:31 +0100

    mobile - buttons adjust

--- src/molecules/footerButtons.tsx
+++ src/molecules/footerButtons.tsx
@@ -3,11 +3,11 @@ import i18n from '../i18n/main';
 import { iFooterButtons } from "../interfaces";
 
 const FooterButtons = (props: iFooterButtons) => {
-    return (<div className="flex justify-center w-full p-8">
+    return (<div className="flex flex-col sm:flex-row justify-center w-full p-8">
         {(props.buttons.includes('save') || props.buttons.includes('nsave')) && <button 
             className={
                 (props.buttons.includes('save') ? "bg-blue-600 hover:bg-blue-700" : "bg-blue-400") 
-                + " text-white py-2 px-4 mx-4 rounded"
+                + " text-white py-2 px-4 my-2 sm:mx-4 rounded"
             }
             disabled={props.buttons.includes('nsave')}
         >
@@ -15,7 +15,7 @@ const FooterButtons = (props: iFooterButtons) => {
         </button>}
 
         {props.buttons.includes('reset') && <button 
-            className="bg-gray-500 hover:bg-gray-700 text-white py-2 px-4 mx-4 rounded"
+            className="bg-gray-500 hover:bg-gray-700 text-white py-2 px-4 my-2 sm:mx-4 rounded"
         >
             {i18n.t('restart')}
         </button>}
