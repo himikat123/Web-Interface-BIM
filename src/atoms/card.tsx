ae43b4d8621a32bd88edc5cf32fa6a780832773a
Alex Him <himikat123@gmail.com>
2023-11-26 11:32:38 +0100

    card paddings fine tune

--- src/atoms/card.tsx
+++ src/atoms/card.tsx
@@ -3,7 +3,7 @@ import { iCard } from "../interfaces";
 
 const Card = (props: iCard) => {
     return (
-        <div className={"border-2 border-menu_light dark:border-menu_dark bg-card_light dark:bg-card_dark rounded-lg p-4"}>
+        <div className={"border-2 border-menu_light dark:border-menu_dark bg-card_light dark:bg-card_dark rounded-lg p-6"}>
             {props.content}
         </div>
     );
