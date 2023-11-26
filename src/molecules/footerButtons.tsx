b3b1a872ddf016c71919c947655792b80e9f0d75
Alex Him <himikat123@gmail.com>
2023-11-26 18:30:26 +0100

    deactivated button color fine tune

--- src/molecules/footerButtons.tsx
+++ src/molecules/footerButtons.tsx
@@ -6,7 +6,7 @@ const FooterButtons = (props: iFooterButtons) => {
     return (<div className="flex flex-col sm:flex-row justify-center w-full p-8">
         {(props.buttons.includes('save') || props.buttons.includes('nsave')) && <button 
             className={
-                (props.buttons.includes('save') ? "bg-blue-600 hover:bg-blue-700" : "bg-blue-400") 
+                (props.buttons.includes('save') ? "bg-blue-600 hover:bg-blue-700" : "bg-blue-200 dark:bg-blue-900 dark:text-blue-600") 
                 + " text-white py-2 px-4 my-2 sm:mx-4 rounded"
             }
             disabled={props.buttons.includes('nsave')}
