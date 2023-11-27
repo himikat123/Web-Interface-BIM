b8a78d74338cdd2ecb546563af144d7b0318d5a1
Alexandru Piteli <piteli@etone.de>
2023-11-27 16:54:39 +0100

    add functions to open/close the restart modal

--- src/modals/restart.tsx
+++ src/modals/restart.tsx
@@ -2,14 +2,16 @@ import React from "react"
 import Button from "../atoms/button";
 import i18n from "../i18n/main";
 
-const ModalRestart = () => {
+const ModalRestart = (props: any) => {
     return <div className="fixed top-0 bottom-0 right-0 left-0 z-50 bg-gray-500 bg-opacity-70 flex justify-center items-center">
         <div className="w-full max-w-2xl bg-page_light dark:bg-page_dark text-text_light dark:text-text_dark border-2 rounded border-menu_light dark:border-menu_dark ">
             
             {/* header */}
             <div className="p-4 border-b border-gray-500 flex justify-between">
                 <div>{i18n.t('confirmation')}</div>
-                <div>X</div>
+                <div onClick={props.modalClose} className="cursor-pointer">
+                    X
+                </div>
             </div>
 
             {/* body */}
@@ -21,12 +23,12 @@ const ModalRestart = () => {
             <div className="flex flex-col sm:flex-row justify-center w-full p-8">
                 <Button className="bg-red-600 hover:bg-red-700 text-text_dark"
                     label={i18n.t('restart')}
-                    onClick={() => {}}
+                    onClick={() => props.modalClose()}
                 />
 
                 <Button className="bg-gray-500 hover:bg-gray-700 text-text_dark "
                     label={i18n.t('cancel')}
-                    onClick={() => {}}
+                    onClick={() => props.modalClose()}
                 />
             </div>
         </div>
--- src/molecules/footerButtons.tsx
+++ src/molecules/footerButtons.tsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import i18n from '../i18n/main';
 import Button from "../atoms/button";
 import ModalRestart from "../modals/restart";
 import { iFooterButtons } from "../interfaces";
 
 const FooterButtons = (props: iFooterButtons) => {
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
+
     return (<>
         <div className="flex flex-col sm:flex-row justify-center w-full p-8">
             {(props.buttons.includes('save') || props.buttons.includes('nsave')) && <Button 
                 className={
                     (props.buttons.includes('save') 
                         ? "bg-blue-600 hover:bg-blue-700 text-text_dark" 
-                        : "bg-blue-200 dark:bg-blue-900 text-blue-100 dark:text-blue-600"
+                        : "bg-blue-200 dark:bg-blue-900 text-blue-100 dark:text-blue-600 cursor-not-allowed"
                     )
                 }
                 disabled={props.buttons.includes('nsave')}
@@ -22,11 +24,11 @@ const FooterButtons = (props: iFooterButtons) => {
             {props.buttons.includes('reset') && <Button 
                 className="bg-red-500 hover:bg-red-700 text-text_dark "
                 label={i18n.t('restart')}
-                onClick={() => {}}
+                onClick={() => {setModalOpen(true)}}
             />}
         </div>
 
-        <ModalRestart />
+        {modalOpen && <ModalRestart modalClose={() => setModalOpen(false)} />}
     </>);
 }
 
