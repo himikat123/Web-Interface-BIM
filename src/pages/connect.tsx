ac7c21c3859daf5bd155e715de4b091e09e3e04b
Alexandru Piteli <piteli@etone.de>
2023-11-30 15:55:46 +0100

    add page connections

--- src/pages/connect.tsx
+++ src/pages/connect.tsx
@@ -3,14 +3,17 @@ import OneColumn from "../templates/oneColumn";
 import { useSelector, useDispatch } from 'react-redux';
 import i18n from '../i18n/main';
 import Card from "../atoms/card";
+import PasswordInput from "../atoms/passwordInput";
 import RadioSwitch from "../atoms/radioSwitch";
 
 const Connect = () => {
     const dispatch = useDispatch();
 
-    const content = <Card content={<>
+    const content = <>
+        <Card content={<>
         
-    </>} />;
+        </>} />
+    </>;
 
     return (<>
         <OneColumn header={i18n.t('connections')} content={content} navbar={true} buttons={['save', 'reset']} />
