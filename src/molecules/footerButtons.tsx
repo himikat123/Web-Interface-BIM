5295ffdd5e8296d4dcd4ea99ee33db3c55f997f7
Alex Him <himikat123@gmail.com>
2023-12-03 10:25:28 +0100

    remove unnecessary

--- src/molecules/footerButtons.tsx
+++ src/molecules/footerButtons.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useSelector } from 'react-redux';
 import i18n from '../i18n/main';
 import Button from "../atoms/button";
@@ -6,14 +6,13 @@ import ModalRestart from "../pages/modalRestart";
 import { iFooterButtons } from "../interfaces";
 
 const FooterButtons = (props: iFooterButtons) => {
-    //const [validd, setValidd] = useState<boolean>(true);
     const [modalOpen, setModalOpen] = useState<boolean>(false);
     const valid = useSelector((state: any) => state.valid);
     const areAllPagesValid = !Object.values(valid).includes(false);
 
     return (<>
         <div className="flex flex-col sm:flex-row justify-center w-full p-8">
-            {(props.buttons.includes('save') || props.buttons.includes('nsave')) && <Button 
+            {(props.buttons.includes('save')) && <Button 
                 className={
                     (areAllPagesValid 
                         ? "bg-blue-600 hover:bg-blue-700 text-text_dark" 
