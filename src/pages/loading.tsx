c44885f12d831fa23aa35b7588fb6a3ce396467a
Alex Him <himikat123@gmail.com>
2023-12-06 19:36:27 +0100

    add margin

--- src/pages/loading.tsx
+++ src/pages/loading.tsx
@@ -4,7 +4,7 @@ import StepsAnimation from '../atoms/stepsAnimation';
 
 const Loading = () => {
     return <div className="flex justify-center items-center w-full h-screen">
-        <div className="border-2 border-menu_light dark:border-menu_dark bg-card_light dark:bg-card_dark rounded-lg p-4 max-w-xl w-full">
+        <div className="border-2 border-menu_light dark:border-menu_dark bg-card_light dark:bg-card_dark rounded-lg p-4 m-4 max-w-xl w-full">
             <h1 className="text-xl text-center mb-8">{i18n.t('loading')}</h1>
             <div className="flex justify-center h-24">
                 <StepsAnimation />
--- src/pages/noConfig.tsx
+++ src/pages/noConfig.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as ErrorSVG } from '../atoms/icons/error.svg';
 
 const NoConfig = () => {
     return <div className="flex justify-center items-center w-full h-screen">
-        <div className="border-2 border-menu_light dark:border-menu_dark bg-card_light dark:bg-card_dark rounded-lg p-4 max-w-xl w-full">
+        <div className="border-2 border-menu_light dark:border-menu_dark bg-card_light dark:bg-card_dark rounded-lg p-4 m-4 max-w-xl w-full">
             <h1 className="text-xl text-center mb-8">{i18n.t('configError')}</h1>
             <div className="flex flex-col justify-center h-36">
                 <ErrorSVG />
