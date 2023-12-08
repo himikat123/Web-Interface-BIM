1e92b11f8a9a6662df1fe2ed13f9cd0568c69f53
Alex Him <himikat123@gmail.com>
2023-12-08 14:39:05 +0100

    modal paddings fine tune

--- src/templates/modal.tsx
+++ src/templates/modal.tsx
@@ -8,7 +8,7 @@ const Modal = (props: iModal) => {
         <div className="mx-4 w-full max-w-2xl h-fit mt-4 md:mt-10 bg-page_light dark:bg-page_dark text-text_light dark:text-text_dark border-2 rounded border-menu_light dark:border-menu_dark">
             
             {/* header */}
-            <div className="p-8 border-b border-gray-500 flex justify-between items-center">
+            <div className="p-4 border-b border-gray-500 flex justify-between items-center">
                 <div>{props.header}</div>
                 <div onClick={props.modalClose} className="p-1 bg-gray-500 hover:bg-gray-700 cursor-pointer border-2 rounded border-menu_light dark:border-menu_dark text-text_dark">
                     <CloseSVG />
@@ -16,19 +16,19 @@ const Modal = (props: iModal) => {
             </div>
 
             {/* body */}
-            <div className="p-8 border-b border-gray-500">
+            <div className="p-4 border-b border-gray-500 overflow-auto" style={{maxHeight: 'calc(100vh - 230px)'}}>
                 {props.content}
             </div>
 
             {/* buttons */}
-            <div className="flex flex-col sm:flex-row justify-center w-full p-8">
-                <Button className="bg-red-600 hover:bg-red-700 text-text_dark"
+            <div className="flex flex-col sm:flex-row justify-center w-full p-4">
+                {props.labelConfirm && <Button className="bg-red-600 hover:bg-red-700 text-text_dark"
                     label={props.labelConfirm}
                     onClick={() => {
                         props.modalClose();
                         props.confirmBtn();
                     }}
-                />
+                />}
 
                 <Button className="bg-gray-500 hover:bg-gray-700 text-text_dark"
                     label={props.labelCancel}
