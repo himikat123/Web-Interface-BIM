12559e0d5c38c1a76e16893698731c5373ad58cd
Alex Him <himikat123@gmail.com>
2023-12-11 19:45:13 +0100

    expand the list of validated sensor readings

--- src/atoms/validateValues.ts
+++ src/atoms/validateValues.ts
@@ -1,12 +1,12 @@
-export const validateTemp = (temp: number): boolean => {
+export const validateTemperature = (temp: number): boolean => {
     return (temp >= -55 && temp <= 125); 
 }
 
-export const validateHum = (hum: number): boolean => {
+export const validateHumidity = (hum: number): boolean => {
     return (hum >= 0 && hum <= 100); 
 }
 
-export const validatePres = (pres: number): boolean => {
+export const validatePressure = (pres: number): boolean => {
     return (pres >= 300 && pres <= 1100); 
 }
 
@@ -14,6 +14,34 @@ export const validateLight = (light: number): boolean => {
     return (light >= 0 && light <= 188000); 
 }
 
-export const validateLowVoltage = (volt: number): boolean => {
-    return (volt >= 0 && volt <= 6);
+export const validateAnalogVoltage = (volt: number): boolean => {
+    return (volt >= 0 && volt <= 5);
+}
+
+export const validateBatteryVoltage = (volt: number): boolean => {
+    return (volt >= 0.6 && volt <= 20); 
+}
+
+export const validateHighVoltage = (volt: number): boolean => {
+    return (volt >= 80 && volt <= 260); 
+}
+
+export const validateCurrent = (curr: number): boolean => {
+    return (curr >= 0 && curr <= 100); 
+}
+
+export const validatePower = (powr: number): boolean => {
+    return (powr >= 0 && powr <= 23000); 
+}
+
+export const validateEnergie = (enrg: number): boolean => {
+    return (enrg >= 0 && enrg <= 10000); 
+}
+
+export const validateFrequency = (freq: number): boolean => {
+    return (freq >= 45 && freq <= 65); 
+}
+
+export const validateS8CO2 = (co2: number): boolean => {
+    return (co2 >= 400 && co2 <= 2000); 
 }
