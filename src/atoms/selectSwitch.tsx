938ba513322b342b8ba09139b3b1a05378584b4f
Alexandru Piteli <piteli@etone.de>
2024-01-11 13:41:29 +0100

    change select to native

--- src/atoms/selectSwitch.tsx
+++ src/atoms/selectSwitch.tsx
@@ -1,69 +1,22 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { ReactComponent as ArrowDownSVG } from '../atoms/icons/arrowDown.svg';
+import React from 'react';
 import { iSelectSwitch } from '../interfaces';
 import "./selectSwitch.scss";
 
 const SelectSwitch = (props: iSelectSwitch) => {
-    function useOutsideAlerter(ref: React.RefObject<HTMLDivElement>) {
-        useEffect(() => {
-            function handleClickOutside(event: TouchEvent | MouseEvent) {
-                if(ref.current && !ref.current.contains(event.target as Node)) setDropdownOpen(false);
-            }
-            document.addEventListener("mousedown", handleClickOutside);
-            return () => {
-                document.removeEventListener("mousedown", handleClickOutside);
-            };
-        }, [ref]);
-    }
-
-    const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
-    const wrapperRef = useRef<HTMLDivElement>(null);
-    useOutsideAlerter(wrapperRef);
-
-    return <> 
-        <select className="form-select" value={props.value ?? 0} >
+    return <div className="relative">
+        <select className="form-select h-14" 
+            value={props.value ?? 0} 
+            onChange={(event: React.ChangeEvent<HTMLSelectElement>) => props.onChange(Number(event.target.value))}
+        >
             {props.options.map((option: string, i: number) => 
-                <option value={i}>{option}</option>
+                <option key={option} value={i}>{option}</option>
             )}
         </select>
 
-        <div ref={wrapperRef}
-            className="relative rounded-md border border-menu_light dark:border-menu_dark p-4"  
-            onClick={() => {setDropdownOpen(!dropdownOpen)}}
-        >
-            <input className="pe-6 h-full w-full rounded-md outline-none bg-card_light dark:bg-card_dark text-text_light dark:text-text_dark cursor-pointer" 
-                value={props.options[props.value ?? 0]} 
-                readOnly
-            />
-
-            <div className="absolute left-2 -top-2 bg-card_light dark:bg-card_dark px-1 text-xs">
-                {props.label}
-            </div>
-
-            <div className={"w-4 absolute top-5 right-3 select-icon cursor-pointer " + (dropdownOpen ? "dropdownOpen" : "")}>
-                <ArrowDownSVG />
-            </div>
-
-            <div className={"absolute w-max top-14 left-0 z-10 select-options overflow-hidden " + (dropdownOpen ? "dropdownOpen" : "")}>
-                <div className="rounded-md border border-menu_light dark:border-menu_dark cursor-pointer overflow-y-scroll">
-                    {props.options.map((option: string, i: number) => {
-                        return <div key={option} 
-                            className={"select-item p-3 first:rounded-t-md last:rounded-b-md " + (props.value === i 
-                                ? "bg-menu_active_light dark:bg-menu_active_dark text-text_dark" 
-                                : "bg-page_light dark:bg-page_dark"
-                            )}
-                            onClick={() => {
-                                props.onChange(i);
-                                setDropdownOpen(false);
-                            }}
-                        >
-                            {option}
-                        </div>
-                    })}
-                </div>
-            </div>
+        <div className="absolute left-2 -top-2 bg-card_light dark:bg-card_dark px-1 text-xs">
+            {props.label}
         </div>
-    </>
+    </div>
 }
 
 export default SelectSwitch;
