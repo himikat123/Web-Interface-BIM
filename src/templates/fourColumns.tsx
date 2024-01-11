981f22745a445c12fe8a11e2e3f8eeb0358020b0
Alexandru Piteli <piteli@etone.de>
2024-01-11 13:48:46 +0100

    fine tune template

--- src/templates/fourColumns.tsx
+++ src/templates/fourColumns.tsx
@@ -7,15 +7,15 @@ const FourColumns = (props: iColumnsTemplate) => {
     return (<div className="flex flex-col min-h-screen">
         {props.navbar && <Navbar />}
 
-        <>{props.content.map((c: React.ReactNode, i: number) => {
+        {props.content.map((c: React.ReactNode, i: number) => {
             return <div key={"cl" + i} className={(props.navbar && i === 0 ? "pt-16 " : "") + "flex flex-col items-center flex-grow"}>
                 {props.header[i] && <h1 className="text-2xl mt-8 mb-4">{props.header[i]}</h1>}
 
-                <div className="max-w-7xl w-full grid grid-cols-1 md:grid-cols-4 gap-4 p-4">
+                <div className="max-w-7xl w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
                     {props.content[i]}
                 </div>
             </div>
-        })}</>
+        })}
 
         {props.footer}
 
