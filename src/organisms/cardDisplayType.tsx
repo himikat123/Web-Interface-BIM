6967693548888fab2050856f0c996298ee8da1b3
Alex Him <himikat123@gmail.com>
2024-01-12 21:47:53 +0100

    add more displays

--- src/organisms/cardDisplayType.tsx
+++ src/organisms/cardDisplayType.tsx
@@ -13,49 +13,42 @@ import Indication from "../atoms/indication";
 const CardDisplayType = (props: iDisplay) => {
     const dispatch = useDispatch();
     const config = useSelector((state: iConfig) => state.config);
-    const displays = [
-        [
-            '--',
-            'NX4832K035', 
-            'NX4832T035',
-            `WS2812b (1 ${i18n.t('ledPerSegment')})`,
-            `WS2812b (2 ${i18n.t('ledsPerSegment')})`,
-            `WS2812b (3 ${i18n.t('ledsPerSegment')})`
-        ],
-        [
-            '--',
-            `WS2812b (1 ${i18n.t('ledPerSegment')})`,
-            `WS2812b (2 ${i18n.t('ledsPerSegment')})`,
-            `WS2812b (3 ${i18n.t('ledsPerSegment')})`
-        ]
+    const displays1 = [
+        { '--': 0 },
+        { 'NX4832K035': 140 }, 
+        { 'NX4832T035': 140 },
+        { 'ILI9341': 100 }
     ];
-
-    const consum = [
-        [
-            0,
-            140,
-            140,
-            1800,
-            3480,
-            5160
-        ],
-        [
-            0,
-            1800,
-            3480,
-            5160
-        ]
+    const displays2 = [
+        { [`TM1637 (4${i18n.t('digits')}, ${i18n.t('withoutSeconds')}, ${i18n.t('points')})`]: 90 },
+        { [`TM1637 (6${i18n.t('digits')}, ${i18n.t('withoutSeconds')}, ${i18n.t('points')})`]: 130 },
+        { [`TM1637 (6${i18n.t('digits')}, ${i18n.t('withSeconds')}, ${i18n.t('points')})`]: 130 },
+        { [`MAX7219 (4${i18n.t('digits')}, ${i18n.t('withoutSeconds')}, ${i18n.t('points')})`]: 90 },
+        { [`MAX7219 (6${i18n.t('digits')}, ${i18n.t('withoutSeconds')}, ${i18n.t('points')})`]: 130 },
+        { [`MAX7219 (6${i18n.t('digits')}, ${i18n.t('withoutSeconds')}, ${i18n.t('dashes')})`]: 130 },
+        { [`MAX7219 (6${i18n.t('digits')}, ${i18n.t('withSeconds')}, ${i18n.t('points')})`]: 130 },
+        { [`MAX7219 (8${i18n.t('digits')}, ${i18n.t('withoutSeconds')}, ${i18n.t('points')})`]: 170 },
+        { [`MAX7219 (8${i18n.t('digits')}, ${i18n.t('withoutSeconds')}, ${i18n.t('dashes')})`]: 170 },
+        { [`MAX7219 (8${i18n.t('digits')}, ${i18n.t('withSeconds')}, ${i18n.t('points')})`]: 170 },
+        { [`MAX7219 (8${i18n.t('digits')}, ${i18n.t('withSeconds')}, ${i18n.t('dashes')})`]: 170 },
+        { [`MAX7219 (${i18n.t('matrices4')}, ${i18n.t('withoutSeconds')}, ${i18n.t('points')})`]: 680 },
+        { [`WS2812b (1 ${i18n.t('ledPerSegment')})`]: 1800 },
+        { [`WS2812b (2 ${i18n.t('ledsPerSegment')})`]: 3480 },
+        { [`WS2812b (3 ${i18n.t('ledsPerSegment')})`]: 5160 },
+        { [`Nixie (4${i18n.t('tubes')})`]: 0 }
     ];
-
+    const displays = props.num == 0 ? [...displays1, ...displays2].map(d => Object.keys(d)[0]) : displays2.map(d => Object.keys(d)[0]);
+    const consums = props.num == 0 ? [...displays1, ...displays2].map(d => Object.values(d)[0]) : displays2.map(d => Object.values(d)[0]);
+    
     return <>
         <Card content={<>
             <SelectSwitch label={i18n.t('displayType')}
-                options={displays[props.num]}
+                options={displays}
                 value={config.display.type[props.num]}
                 onChange={(val: number) => dispatch(cf.DisplayTypeChange({num: props.num, val: val}))}
             />
 
-            {displays[props.num][config.display.type[props.num]].includes('WS2812b') && <div>
+            {displays[config.display.type[props.num]]?.includes('WS2812b') && <div>
                 <div className="mt-4 mb-1 text-xs">{i18n.t('sacrificial')}</div>
                 <Toggle checked={config.display.sled[props.num]}
                     onChange={() => dispatch(cf.DisplaySledChange({num: props.num, val: config.display.sled[props.num] ? 0 : 1}))}
@@ -92,12 +85,12 @@ const CardDisplayType = (props: iDisplay) => {
                     {i18n.t('maximumDisplayCurrent')}:
                     <Indication error={false} 
                         value={String(
-                            displays[props.num][config.display.type[props.num]].includes('WS2812b') 
-                                ? (Math.round(consum[props.num][config.display.type[props.num]] 
+                            displays[config.display.type[props.num]].includes('WS2812b') 
+                                ? (Math.round(consums[config.display.type[props.num]] 
                                     * config.display.brightness.max[props.num] 
                                     / 255
                                 ) + config.display.sled[props.num])
-                                : consum[props.num][config.display.type[props.num]]
+                                : consums[config.display.type[props.num]]
                         ) + i18n.t('units.ma')} 
                     />
                 </div>
