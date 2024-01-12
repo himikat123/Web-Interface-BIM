1fcd21860d3cc2638127320aff47f0c84448b5df
Alexandru Piteli <piteli@etone.de>
2024-01-12 16:52:08 +0100

    fix label shift down

--- src/atoms/timeInput.tsx
+++ src/atoms/timeInput.tsx
@@ -4,7 +4,7 @@ import { iTimeInput } from '../interfaces';
 const TimeInput = (props: iTimeInput) => {
     return <> 
         <div className="group relative h-14 w-full rounded-md border focus-within:border-primary focus-within:ring-2 focus-within:ring-primary border-menu_light dark:border-menu_dark">
-            <label className="absolute left-2 -top-1 z-0 -translate-y-1 bg-card_light dark:bg-card_dark px-1 text-base pointer-events-none duration-200 group-focus-within:top-0 group-focus-within:text-xs text-xs">
+            <label className="absolute left-2 -top-1 z-0 -translate-y-1 bg-card_light dark:bg-card_dark px-1 text-base pointer-events-none duration-200 text-xs">
                 {props.label}
             </label>
             <input className="z-10 h-full w-full rounded-md px-3.5 py-4 outline-none bg-card_light dark:bg-card_dark text-text_light dark:text-text_dark" 
