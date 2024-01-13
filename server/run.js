1d281e7f0be43b785cf2456ad391d3d1e4f5f4cc
Alex Him <himikat123@gmail.com>
2024-01-13 20:56:30 +0100

    add debug info on the server side

--- server/run.js
+++ server/run.js
@@ -8,13 +8,13 @@ const data = require('./data');
 app.use(bodyParser.text({ type: "*/*" }));
 
 app.get('/esp/restart', (req, res) => {
-    console.log('GET /esp/restart');
+    console.log('GET /esp/restart', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.post("/esp/saveConfig", (req, res) => {
-    console.log('POST /esp/saveConfig');
+    console.log('POST /esp/saveConfig', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     let data = req.body.replace('config:', '');
     setTimeout(() => {
@@ -33,110 +33,110 @@ app.get('/data.json', (req, res) => {
 });
 
 app.get('/esp/bright', (req, res) => {
-    console.log('GET /esp/bright');
+    console.log('GET /esp/bright', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.get('/esp/sensitivity', (req, res) => {
-    console.log('GET /esp/sensitivity');
+    console.log('GET /esp/sensitivity', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.get('/esp/dispToggle', (req, res) => {
-    console.log('GET /esp/dispToggle');
+    console.log('GET /esp/dispToggle', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.get('/esp/brightLimit', (req, res) => {
-    console.log('GET /esp/brightLimit');
+    console.log('GET /esp/brightLimit', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.get('/esp/color', (req, res) => {
-    console.log('GET /esp/color');
+    console.log('GET /esp/color', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.get('/esp/syncClock', (req, res) => {
-    console.log('GET /esp/syncClock');
+    console.log('GET /esp/syncClock', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.get('/esp/syncdialog', (req, res) => {
-    console.log('GET /esp/syncDialog');
+    console.log('GET /esp/syncDialog', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("22:35");
 });
 
 app.post('/esp/changePass', (req, res) => {
-    console.log('GET /esp/changePass');
+    console.log('GET /esp/changePass', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.post('/esp/fileUpload', (req, res) => {
-    console.log('POST /esp/fileUpload');
+    console.log('POST /esp/fileUpload', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.post('/esp/rename', (req, res) => {
-    console.log('POST /esp/rename');
+    console.log('POST /esp/rename', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.post('/esp/delete', (req, res) => {
-    console.log('POST /esp/delete');
+    console.log('POST /esp/delete', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.post('/esp/fwUpdate', (req, res) => {
-    console.log('POST /esp/fwUpdate');
+    console.log('POST /esp/fwUpdate', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.get('/esp/login', (req, res) => {
-    console.log('GET /esp/login');
+    console.log('GET /esp/login', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
     //res.send("error");
 });
 
 app.get('/esp/volume', (req, res) => {
-    console.log('GET /esp/volume');
+    console.log('GET /esp/volume', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.get('/esp/equalizer', (req, res) => {
-    console.log('GET /esp/equalizer');
+    console.log('GET /esp/equalizer', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.get('/esp/mp3play', (req, res) => {
-    console.log('GET /esp/mp3play');
+    console.log('GET /esp/mp3play', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.get('/esp/mp3stop', (req, res) => {
-    console.log('GET /esp/mp3stop');
+    console.log('GET /esp/mp3stop', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
 
 app.get('/esp/animation', (req, res) => {
-    console.log('GET /esp/animation');
+    console.log('GET /esp/animation', req.query);
     res.set('Access-Control-Allow-Origin', '*');
     res.send("OK");
 });
