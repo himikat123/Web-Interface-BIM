42bd2119f1954531fc507e1fd3637c02e2a4df03
Alex Him <himikat123@gmail.com>
2024-01-23 22:14:57 +0100

    remove unnecessary imports

--- src/pages/clock.tsx
+++ src/pages/clock.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import i18n from '../i18n/main';
 import ThreeColumns from "../templates/threeColumns";
 import CardClockTime from "../organisms/cardClockTime";
