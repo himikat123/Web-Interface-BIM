315435fcf3eba824dfca6a2b29411c9da2bcbe9b
Alex Him <himikat123@gmail.com>
2024-02-29 14:42:08 +0100

    mobile fine tuning

--- src/atoms/card.tsx
+++ src/atoms/card.tsx
@@ -5,7 +5,7 @@ import "./card.scss";
 const Card = (props: iCard) => {
     return (
         <div className={"card border-2 border-menu_light dark:border-menu_dark bg-card_light dark:bg-card_dark rounded-lg p-6 pb-10 " + (props.className ?? '')}>
-            {props.header && <h1 className="text-xl text-center mb-8 select-none">{props.header}</h1>}
+            {props.header && <h1 className="text-xl text-center mb-8 sm:mt-4 select-none">{props.header}</h1>}
 
             {props.content}
         </div>
