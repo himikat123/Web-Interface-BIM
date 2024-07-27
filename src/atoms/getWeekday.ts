e071db202a39d17e43e450857f49b3a21b11f4c5
Alex Him <himikat123@gmail.com>
2024-07-27 22:23:17 +0200

    fix weekday utc offset

--- src/atoms/getWeekday.ts
+++ src/atoms/getWeekday.ts
@@ -2,6 +2,6 @@ import i18n from "../i18n/main";
 import moment from "moment";
 
 export default function getWeekday(time: number): string {
-    const day = moment(time * 1000).day();
+    const day = moment(time * 1000).utc().day();
     return i18n.t(`weekday.${day}`);
 }
