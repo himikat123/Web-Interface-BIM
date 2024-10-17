2a56de2397c1480deda1f2bcca9ebab0cac357c0
Alex Him <himikat123@gmail.com>
2024-10-17 11:13:40 +0200

    fix update symbol in weather update time

--- src/atoms/canvas/lcdShowUpdTime.ts
+++ src/atoms/canvas/lcdShowUpdTime.ts
@@ -1,6 +1,7 @@
 import store from '../../redux/store';
 import moment from "moment";
-import { printText } from "./primitives";
+import { printText, drawScaledImage } from "./primitives";
+import * as symbols from "../img/symbols";
 
 export default function lcdShowUpdTime(ctx: CanvasRenderingContext2D, 
     prevTime: number | undefined, color: string, bgColor: string
@@ -11,11 +12,14 @@ export default function lcdShowUpdTime(ctx: CanvasRenderingContext2D,
         const model = store.getState().config.display.model[0];
         const dispModel = (model === 0 || model === 1) ? 0 : 1;
         const upd = time > 0
-            ? moment.unix(time).utc().format(dispModel ? '⭮ DD.MM.YYYY HH:mm' : '⭮ DD.MM.YYYY HH:mm :ss')
+            ? moment.unix(time).utc().format(dispModel ? 'DD.MM.YYYY HH:mm' : 'DD.MM.YYYY HH:mm :ss')
             : '';
-        const x = dispModel ? 150 : 188;
+        const x = dispModel ? 140 : 188;
+        const c = dispModel ? 270 : 320;
 
-        printText(ctx, x, 146, 146, 16, upd, 14, 'center', color, bgColor);
+        printText(ctx, x, 146, 146, 16, upd, 14, 'right', color, bgColor);
+        const w = ctx.measureText(upd).width;
+        if(w) drawScaledImage(ctx, symbols.upd(), c - w, 146, 12, 12);
     }
 
     return time;
