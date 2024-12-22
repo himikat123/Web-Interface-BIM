683a09c4c064ae3e91e00be2315b3bb9a02c0a48
Alex Him <himikat123@gmail.com>
2024-12-22 18:09:56 +0100

    fix redirect for non-logged in user

--- src/dataFetch.ts
+++ src/dataFetch.ts
@@ -3,17 +3,20 @@ import { dataFetchingChange, dataStateChange, setDataState, updateDataChange } f
 import store from "./redux/store";
 import hostUrl from "./atoms/hostUrl";
 
-export default function dataFetch(path: string): string {
+export default async function dataFetch(path: string): Promise<string> {
     let navigate = '';
 
-    store.dispatch(dataFetchingChange(true));
-    axios(`${hostUrl()}/data.json?code=${localStorage.getItem('code') || '0'}`)
-    .then(res => {
+    try {
+        store.dispatch(dataFetchingChange(true));
+        const res = await axios(`${hostUrl()}/data.json?code=${localStorage.getItem('code') || '0'}`);
+        
         store.dispatch(dataStateChange('ok'));
         store.dispatch(dataFetchingChange(false));
         store.dispatch(updateDataChange(false));
+
         let resData = { ...res.data };
-        if(res.data.state === 'DEMO') {
+
+        if (res.data.state === 'DEMO') {
             const date = new Date();
             const timezoneOffset = date.getTimezoneOffset();
             resData.time = Math.round(Date.now() / 1000 - timezoneOffset * 60);
@@ -23,16 +26,17 @@ export default function dataFetch(path: string): string {
             resData.weather.time = Math.round(resData.time - 15 * 60);
             resData.thing.time = Math.round(resData.time - 7 * 60);
         }
-        if(res.data.state === 'OK') if(path === '/login') navigate = '/';
-        if(res.data.state === 'LOGIN') if(path !== '/login') navigate = '/login';
+
+        if (res.data.state === 'OK' && path === '/login') navigate = '/';
+        if (res.data.state === 'LOGIN' && path !== '/login') navigate = '/login';
+
         store.dispatch(setDataState(resData));
-    })
-    .catch(err => {
+    } catch (err) {
         store.dispatch(dataStateChange('error'));
         console.error(err);
         store.dispatch(dataFetchingChange(false));
         store.dispatch(updateDataChange(false));
-    });
+    }
 
     return navigate;
 }
