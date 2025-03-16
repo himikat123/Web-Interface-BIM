86511ac3bb1ddb7b0ac821ba48d9614987535e63
Alex Him <himikat123@gmail.com>
2025-03-16 12:48:49 +0100

    display IP address instead of weather update time

--- src/atoms/canvas/lcdShowUpdTime.ts
+++ src/atoms/canvas/lcdShowUpdTime.ts
@@ -8,6 +8,8 @@ export default function lcdShowUpdTime(ctx: CanvasRenderingContext2D,
     prevTime: number | undefined, color: string, bgColor: string
 ): number {
     const time = store.getState().data.weather.time;
+    const ip = store.getState().data.network.ip;
+    const numIP = (!/^\d+\.\d+\.\d+\.\d+$/.test(ip)) ? 0 : Number(ip.split('.').map(Number).join(''));
     let hourFormat;
     switch(store.getState().config.clock.format) {
         case 0: hourFormat = 'h'; break;
@@ -16,7 +18,7 @@ export default function lcdShowUpdTime(ctx: CanvasRenderingContext2D,
         default: hourFormat = 'HH'; break;
     }
 
-    if(time !== prevTime) {
+    if((device() === 'WeatherMonitorBIM32') ? (time !== prevTime) : (numIP !== prevTime)) {
         const model = store.getState().config.display.model[0];
         const dispModel = (model === 0 || model === 1) ? 0 : 1;
         const upd = time > 0
@@ -27,10 +29,15 @@ export default function lcdShowUpdTime(ctx: CanvasRenderingContext2D,
         const x = dispModel ? (device() === 'WeatherMonitorBIM32' ? 140 : 173) : 188;
         const c = dispModel ? (device() === 'WeatherMonitorBIM32' ? 270 : 303) : 320;
 
-        printText(ctx, x, 146, 146, 16, upd, 14, 'right', color, bgColor);
-        const w = ctx.measureText(upd).width;
-        if(w) drawScaledImage(ctx, symbols.upd(), c - w, 146, 12, 12);
+        if(device() === 'WeatherMonitorBIM32') {
+            printText(ctx, x, 146, 146, 16, upd, 14, 'right', color, bgColor);
+            const w = ctx.measureText(upd).width;
+            if(w) drawScaledImage(ctx, symbols.upd(), c - w, 146, 12, 12);
+        }
+        else {
+            printText(ctx, x, 146, 146, 16, ip, 14, 'right', color, bgColor);
+        }
     }
 
-    return time;
+    return (device() === 'WeatherMonitorBIM32') ? time : numIP;
 }
