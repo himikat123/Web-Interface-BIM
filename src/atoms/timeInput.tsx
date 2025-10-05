2fc25fe6571cc62541522797d10328ee26562349
himikat123 <himikat123@gmail.com>
2025-10-05 10:41:21 +0200

    Fix for time shift in input field on iOS

--- src/atoms/timeInput.tsx
+++ src/atoms/timeInput.tsx
@@ -6,7 +6,7 @@ export default function TimeInput(props: iTimeInput) {
         <label className="absolute left-2 -top-1 z-0 -translate-y-1 bg-card_light dark:bg-card_dark px-1 text-base pointer-events-none duration-200 text-xs">
             {props.label}
         </label>
-        <input className="z-10 h-full w-full rounded-md px-3.5 py-4 outline-none bg-card_light dark:bg-card_dark text-text_light dark:text-text_dark" 
+        <input className="z-10 h-full w-full rounded-md px-3.5 outline-none bg-card_light dark:bg-card_dark text-text_light dark:text-text_dark" 
             type="time"
             step={props.step}
             value={props.value ?? 0}
