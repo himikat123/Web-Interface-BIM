bee0d11aa7f5374cf959dc23b33a2f26ff36f869
himikat123 <himikat123@gmail.com>
2026-06-25 17:37:15 +0200

    font fine tune

--- src/atoms/canvas/lcdShowTemperature.ts
+++ src/atoms/canvas/lcdShowTemperature.ts
@@ -24,9 +24,9 @@ export function lcdShowTemperatureInside(
     const temp = lcdGetTempIn(sequence ?? 0, config, data);
 
     if(temp !== prevTemp) {
-        let x = 258, y = 68, w = 101, h = 38, f = 44; // NX4832K(T)035
+        let x = 258, y = 68, w = 101, h = 38, f = 42; // NX4832K(T)035
         switch(dispModel) {
-            case D.NX4827K043: x = 252; y = 54; w = 101; h = 36; f = 40; break;
+            case D.NX4827K043: x = 252; y = 54; w = 101; h = 36; f = 38; break;
             case D.ILI9341: x = 173; y = 53; w = 70; h = 26; f = 29; break;
         }
         showTemperature(ctx, temp, x, y, w, h, f, color, bgColor);
@@ -41,9 +41,9 @@ export function lcdShowTemperatureOutside(
     const temp = lcdGetTempOut(config, data);
 
     if(temp !== prevTemp) {
-        let x = 112, y = 154, w = 107, h = 44, f = 44, xi = 99, yi = 140, wi = 13, hi = 48; // NX4832K(T)035
+        let x = 112, y = 154, w = 107, h = 44, f = 42, xi = 99, yi = 140, wi = 13, hi = 48; // NX4832K(T)035
         switch(dispModel) {
-            case D.NX4827K043: x = 106; y = 122; w = 106; h = 36; f = 40; xi = 84; yi = 112; wi = 13; hi = 40; break;
+            case D.NX4827K043: x = 106; y = 122; w = 106; h = 36; f = 38; xi = 84; yi = 112; wi = 13; hi = 40; break;
             case D.ILI9341: x = 71; y = 113; w = 70; h = 26; f = 29; xi = 61; yi = 104; wi = 9; hi = 33; break;
         }
 
